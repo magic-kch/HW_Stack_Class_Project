48b93860a19a36958326c7508f9e0d08f6fe7390
Magic.kch <magic.kch@yandex.ru>
2024-08-01 17:43:34 +0300

    add README.md

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return len(self.items) == 0
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        if not self.isEmpty():
-            return self.items.pop()
-        else:
-            raise IndexError("pop from an empty stack")
-
-    def peek(self):
-        if not self.isEmpty():
-            return self.items[-1]
-        else:
-            return None
-
-    def size(self):
-        return len(self.items)
+from my_class import Stack
 
 
 def is_balanced(expression):
